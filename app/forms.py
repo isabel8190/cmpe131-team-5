4d1fd70d549ab99476a8476c2bcf44c70e7cdb1a
Isabel Luong <isabel.luong@sjsu.edu>
2022-12-03 11:17:25 -0800

    added signup form

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,10 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign In')
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+#class Message(FlaskForm):
