7343f7c843b9d7f0fa9b3c6c0966ca96a6b9e946
Isabel Luong <isabel.luong@sjsu.edu>
2022-12-04 16:07:20 -0800

    added comments and EditProfileForm for profile editing

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, FileField
 from wtforms.validators import DataRequired, EqualTo, ValidationError
 
 from app.models import User
 
+#form for login
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign In')
 
+#form for account creation
 class SignupForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
-    confirmPassword = PasswordField('Confirm Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
@@ -21,4 +23,27 @@ class SignupForm(FlaskForm):
         if user is not None:        #username matches with one in a database
             raise ValidationError("Username already taken.")
 
-#class Message(FlaskForm):
+#form for home page posts               <--- just beginning, not completed
+class PostForm(FlaskForm):
+    message = TextAreaField('Enter a message', validators = [DataRequired()])
+    post = SubmitField('Post')
+
+#form for profile editing
+class EditProfileForm(FlaskForm):
+    picture = FileField('Profile Picture')
+    newUsername = StringField('New Username')
+    newPassword = PasswordField('New Password')
+    confirmPassword = PasswordField('Confirm Changes Using Password', validators=[DataRequired()])
+    #bio = TextAreaField('Bio', validators=[Length(min=0, max=250)]) #max bio length 250 char
+    submit = SubmitField('Confirm')
+
+    def validate_newUsername(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Choose another username.')
+            
+
+#form for private messaging
+class Message(FlaskForm):
+    message = StringField('Enter a message', validators = [DataRequired()])
+    enter = SubmitField('Enter')
