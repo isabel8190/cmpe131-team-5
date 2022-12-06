1228a8498567727bbbf111b0ef91a18c750c7d7c
Bhargavi Datye <datye.bhargavi@gmail.com>
2022-12-05 23:30:26 -0500

    working on private messaging

--- app/models.py
+++ app/models.py
@@ -64,6 +64,17 @@ class Message(db.Model):
 
     def __repr__(self):
         return 
+
+class Private_Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
+
 '''
 class Post(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
