d92b471cffcc067a812224188f929beb117c56a1
Isabel Luong <isabel.luong@sjsu.edu>
2022-12-06 18:22:53 -0800

    working on search user, finalized edit profile

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,9 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-#from config import Config
-#from flask_migrate import Migrate
+from flask_migrate import Migrate
 
 myapp_obj = Flask(__name__)
-#myapp_obj.config.from_object(Config)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,7 +15,7 @@ myapp_obj.config.update(                    #app.config.from_mapping(
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 )
 db = SQLAlchemy(myapp_obj)
-#migrate = Migrate(myapp_obj,db)
+migrate = Migrate(myapp_obj,db)
 
 login = LoginManager(myapp_obj)
 
--- app/forms.py
+++ app/forms.py
@@ -30,11 +30,11 @@ class PostForm(FlaskForm):
 
 #form for profile editing
 class EditProfileForm(FlaskForm):
-    picture = FileField('Profile Picture')
+    #picture = FileField('Profile Picture')
     newUsername = StringField('New Username')
     newPassword = PasswordField('New Password')
     confirmPassword = PasswordField('Confirm Changes Using Password', validators=[DataRequired()])
-    #newBio = TextAreaField('Bio', validators=[Length(min=0, max=250)]) #max bio length 250 char
+    newBio = TextAreaField('Bio', validators=[Length(min=0, max=250)]) #max bio length 250 char
     submit = SubmitField('Confirm')
 
     def validate_newUsername(self, username):
@@ -42,8 +42,16 @@ class EditProfileForm(FlaskForm):
         if user is not None:
             raise ValidationError('Choose another username.')
             
+class SearchForm(FlaskForm):
+    search = StringField('Search For User', validators = [DataRequired()])
+    submit = SubmitField('Search')
+
+    def validate_search(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user is None:        #username does not match with one in a database
+            raise ValidationError("There is no such user.")
 
 #form for private messaging
-class Message(FlaskForm):
+class MessageForm(FlaskForm):
     message = StringField('Enter a message', validators = [DataRequired()])
     enter = SubmitField('Enter')
