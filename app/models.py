fc3eabd017b296449d1d130696dbeef6828f960a
Isabel Luong <isabel.luong@sjsu.edu>
2022-12-08 06:58:02 -0800

    updated readme.md

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import login
 from flask_login import UserMixin
 
-from datetime import datetime
+#from datetime import datetime
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
